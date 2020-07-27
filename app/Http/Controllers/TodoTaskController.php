e4bac78b21c602e5ce4f63fcc8eaf2c940f21a63
Arnas <1arnasdickus1@gmail.com>
2020-07-27 16:46:14 +0300

    fix bug

--- app/Http/Controllers/TodoTaskController.php
+++ app/Http/Controllers/TodoTaskController.php
@@ -8,6 +8,16 @@ use Illuminate\Http\Request;
 class TodoTaskController extends Controller
 {
     /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index()
+    {
+        return $tasks = TodoTask::orderBy('created_at', 'asc')->get();
+    }
+
+    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
